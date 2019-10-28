ce1fffd8b9666e887deee1d613ea32e7d1e711cc
vdrn <vburdzovic@gmail.com>
2019-10-28 12:06:04 -0400

    Throw http 404 not found when no route matches

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import HttpException from 'http-exception'
 import routers from './routers'
 import pkg from '../package.json'
 
@@ -7,6 +8,9 @@ console.log(`Launching ${pkg.name} v${pkg.version}`)
 const app = express()
 
 app.use('/', ...routers)
+app.use(() => {
+  throw HttpException.notFound()
+})
 
 app.listen(8080, () =>
   console.log(`App ${pkg.name} started listening on port 8080`)
