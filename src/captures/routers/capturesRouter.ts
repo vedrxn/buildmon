054860681eb6a998d40918ee1efd759acfeec05d
vdrn <vburdzovic@gmail.com>
2019-11-02 22:21:29 -0400

    Implement get captures endpoint

--- src/captures/routers/capturesRouter.ts
+++ src/captures/routers/capturesRouter.ts
@@ -1,3 +1,21 @@
 import express from 'express'
+import HttpException from 'http-exception'
+import { Db } from '../../db'
 
-export default express.Router()
+const router = express.Router()
+
+router
+  .route('/captures')
+  .get((req, res, next) => {
+    const db: Db = req.app.get('db')
+
+    db.captures.find({}, (error, docs) => {
+      if (error) {
+        return next(HttpException.internalServerError())
+      }
+
+      res.json(docs)
+    })
+  })
+
+export default router
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,11 @@
 import DataStore from 'nedb'
 
 export interface Db {
+  captures: Nedb
   stats: Nedb
 }
 
 export const createDb = (): Db => ({
+  captures: new DataStore(),
   stats: new DataStore()
 })
